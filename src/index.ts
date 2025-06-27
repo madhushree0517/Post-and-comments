fcddb64671b7b295aade0e5cd923e035953ae56a
Madhushree0517 <madhushree0517@gmail.com>
2025-06-27 15:38:58 +0530

    chore: trigger redeploy

--- src/index.ts
+++ src/index.ts
@@ -47,7 +47,7 @@ app.delete('/comments/:id', async (req, res) => {
 })
 
 // src/index.ts
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
